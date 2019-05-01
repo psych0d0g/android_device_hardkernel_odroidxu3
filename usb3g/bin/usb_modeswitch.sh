7b0ac08fe12f8b41ee300978364e4c5e7fb39c08
psych0d0g <lukas.wingerberg@springernature.com>
2019-05-01 19:48:17 +0200

    fix some path names to hopefully get usb_modeswitch to work natively at boot time

--- usb3g/bin/usb_modeswitch.sh
+++ usb3g/bin/usb_modeswitch.sh
@@ -4,8 +4,10 @@ echo "Calling modeswitch when a compatible usb modem is attached"
 
 for i in $(lsusb | cut -d " " -f 6 | tr \: _)
 do
-	if test -f "/system/etc/usb_modeswitch/${i}"
+	if test -f "/system/etc/usb_modeswitch.d/${i}"
 	then
-		usb_modeswitch -c /system/etc/usb_modeswitch/${i}
+		VID=$(echo ${i} | cut -f1 -d:)
+		PID=$(echo ${i} | cut -f2 -d:)
+		usb_modeswitch -v ${VID} -p ${PID} -c /system/etc/usb_modeswitch.d/${i}
 	fi
 done

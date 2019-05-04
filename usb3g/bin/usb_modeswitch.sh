5b7b0cc6a654d1e58e8a0d1e7ee38f29325b70a3
psych0d0g <lukas.wingerberg@springernature.com>
2019-05-04 11:00:19 +0200

    call modeswitch via init.d support

--- usb3g/bin/usb_modeswitch.sh
+++ usb3g/bin/usb_modeswitch.sh
@@ -1,13 +1,16 @@
 #!/system/xbin/bash
 
 echo "Calling modeswitch when a compatible usb modem is attached"
-
-for i in $(lsusb | cut -d " " -f 6 | tr \: _)
-do
-	if test -f "/system/etc/usb_modeswitch.d/${i}"
-	then
-		VID=$(echo ${i} | cut -f1 -d:)
-		PID=$(echo ${i} | cut -f2 -d:)
-		usb_modeswitch -v ${VID} -p ${PID} -c /system/etc/usb_modeswitch.d/${i}
-	fi
-done
+ether_disabled=$(getprop ro.disable.ethernet)
+if [[ "${ether_disabled}" -eq 1 ]]
+then
+	for device in $(lsusb | cut -d " " -f 6 | tr \: _)
+	do
+		if test -f "/system/etc/usb_modeswitch.d/${device}"
+		then
+			VID=$(echo ${i} | cut -f1 -d_)
+			PID=$(echo ${i} | cut -f2 -d_)
+			usb_modeswitch -v ${VID} -p ${PID} -c /system/etc/usb_modeswitch.d/${device}
+		fi
+	done
+fi
